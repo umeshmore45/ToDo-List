35056082788a2a9a7940ad886626192e0ab8db65
Trainee 29 <umesh.more@raweng.com>
2020-12-06 01:11:00 +0530

    local storage

--- src/compontes/local.js
+++ src/compontes/local.js
@@ -1,11 +1,3 @@
-// export const getLocal = async () => {
-//   let local = await JSON.parse(localStorage.getItem("task"));
-//   //   let li = document.createElement("li");
-//   //   li.innerHTML = "hello";
-//   //   return li;
-//   return local;
-// };
-// export let local = JSON.parse(localStorage.getItem("task"));
 import { ToDoData } from "../todolistdata.js";
 
 export let data = JSON.parse(localStorage.getItem("data") || "[]");
